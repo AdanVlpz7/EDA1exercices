70adcbc571fc8e9e56ea619e2de51f5fd53b5d9c
AdanVlpz7 <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-21 17:11:00 -0600

    Ejercicio 1 final

--- practica3/Ej1.h
+++ practica3/Ej1.h
@@ -5,12 +5,12 @@ struct _NUM_COMPLEJOS{
   
     float real;
     float complejo;
-    //char c;
 };
 
 typedef struct _NUM_COMPLEJOS COMPLEJO;
 
 void suma(COMPLEJO *t1, COMPLEJO *t2, COMPLEJO* resultado);
 void multiplicacion(COMPLEJO *t1, COMPLEJO *t2, COMPLEJO* resultado);
-void imprimir(COMPLEJO c1);
+void leerComplejo(COMPLEJO *c);
+void imprimir(COMPLEJO *c1);
 #endif
